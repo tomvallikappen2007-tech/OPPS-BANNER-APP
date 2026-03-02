5e9f6720393daf8533222827aac5c7fb2db655a9
tomvallikappen2007-tech <tomvallikappen2007@gamil.com>
2026-03-02 23:49:57 +0530

    UC1: print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPS Banner App
+ *
+ * UC1: Print OOPS to Console
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
